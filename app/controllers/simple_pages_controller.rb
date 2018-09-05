45fc15b2b4c086472f983a71512274f27b566206
Samuel Sadler <fustigata@gmail.com>
2018-09-05 16:05:40 +0200

    added slideshow, products broken

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -7,4 +7,7 @@ class SimplePagesController < ApplicationController
   def landing_page
     @featured_product = Product.first
   end
+  def landing_page
+    @products = Product.limit(3)
+  end
 end
