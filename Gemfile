6da3d5e13f9b3a77b66df2fe5cbf63d6ea604bf7
Samuel Sadler <fustigata@gmail.com>
2018-10-05 17:23:50 +0200

    added CanCanCan

--- Gemfile
+++ Gemfile
@@ -64,3 +64,5 @@ group :development, :test do
 end
 
 gem 'devise'
+
+gem 'cancancan', '~> 1.10'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
+  load_and_authorize_resource
 
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to main_app.root_url, alert: exception.message
+  end
   # GET /users
   # GET /users.json
   def index
