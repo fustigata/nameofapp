32d7786a6bf7f12c0fad537d6d9d551ce499626b
Samuel Sadler <fustigata@gmail.com>
2018-10-12 15:42:04 -0400

    Added star ratings

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,12 @@ class Product < ApplicationRecord
   def self.search(search_term)
     Product.where("name LIKE ?", "%#{search_term}%")
   end
+
+  def highest_rating_comment
+    comments.rating_desc.first
+  end
+
+  def average_rating
+    comments.average(:rating).to_f
+  end
 end
