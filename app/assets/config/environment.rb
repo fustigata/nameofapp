6efaac119f52cb052bfcc313ccdb48559cedb16d
Samuel Sadler <fustigata@gmail.com>
2018-11-08 15:32:32 +0100

    tried fixing sendgrid

--- app/assets/config/environment.rb
+++ app/assets/config/environment.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
   address: 'smtp.sendgrid.net',
   port: '587',
   authentication: :plain,
-  user_name: ENV['SENDGRID_USERNAME'],
-  password: ENV['SENDGRID_PASSWORD'],
+  user_name: ENV['app105794500@heroku.com'],
+  password: ENV['piszkim23805'],
   domain: 'heroku.com',
   enable_starttls_auto: true
 }
