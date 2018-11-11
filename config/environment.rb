29eaa28930c62fda9dc065e5451675b6b00f657f
Samuel Sadler <fustigata@gmail.com>
2018-11-11 18:31:23 +0100

    tried fixing mailgun

--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,7 @@ ActionMailer::Base.smtp_settings = {
   :address        => ENV['MAILGUN_SMTP_SERVER'],
   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain         => 'yourapp.heroku.com',
+  :domain         => 'samdcptest.heroku.com',
   :authentication => :plain,
 }
 ActionMailer::Base.delivery_method = :smtp
