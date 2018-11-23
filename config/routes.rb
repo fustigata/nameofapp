2641480715f26f40e5d05c4b2cc1a3de32c058c7
Samuel Sadler <fustigata@gmail.com>
2018-11-23 16:54:37 +0100

    fixed stripe routing error

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout' }
   resources :users
   resources :products do
-    resources :comments
+  resources :comments
   end
   resources :orders, only: [:index, :show, :create, :destroy]
   get 'simple_pages/about'
@@ -10,5 +10,6 @@ Rails.application.routes.draw do
   get 'simple_pages/index'
   root 'simple_pages#landing_page'
   post 'simple_pages/thank_you'
+  post 'payments/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
