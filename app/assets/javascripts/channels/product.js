57bdbbbe8df7171f249bf979c9ed55e58bce08d0
Samuel Sadler <fustigata@gmail.com>
2018-12-03 16:11:32 +0100

    Still cant push to heroku

--- app/assets/javascripts/channels/product.js
+++ app/assets/javascripts/channels/product.js
@@ -10,6 +10,8 @@ App.product = App.cable.subscriptions.create("ProductChannel", {
   received: function(data) {
     // Called when there's incoming data on the websocket for this channel
     $(".alert.alert-info").show();
+    console.log(data);
+    ProductChannel.broadcast_to @product.id, comment: CommentsController.render(partial: 'comments/comment', locals: {comment: @comment}), average_rating: @product.average_rating
   }
   listen_to_comments: function() {
     return this.perform('listen', {
--- app/channels/product_channel.rb
+++ app/channels/product_channel.rb
@@ -8,7 +8,7 @@ class ProductChannel < ApplicationCable::Channel
     # Any cleanup needed when channel is unsubscribed
   end
 
-  def listen (:data)
+  def listen (data)
     stop_all_streams
     stream_for data["product_id"]
   end
